17a9efbdce9b67c6b8c17d7582ab04efa128e550
lllllinalll <polina.romanenko.2005@bk.ru>
2024-09-26 14:01:36 +0700

    фывфыв

--- ProcessDispatcher/Form1.Designer.cs
+++ ProcessDispatcher/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            number = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            id = new DataGridViewTextBoxColumn();
+            usedRAM = new DataGridViewTextBoxColumn();
+            startTime = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(523, 11);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Получить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += GetProcessButton_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(604, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Обновить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += RefreshProcessListButton_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(685, 11);
+            button3.Name = "button3";
+            button3.Size = new Size(103, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Убить процесс";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += KillProcessButton_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { number, name, id, usedRAM, startTime });
+            dataGridView1.Location = new Point(12, 40);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 398);
+            dataGridView1.TabIndex = 3;
+            // 
+            // number
+            // 
+            number.HeaderText = "Номер";
+            number.Name = "number";
+            number.ReadOnly = true;
+            // 
+            // name
+            // 
+            name.HeaderText = "Имя процесса";
+            name.Name = "name";
+            name.ReadOnly = true;
+            // 
+            // id
+            // 
+            id.HeaderText = "ID процесса";
+            id.Name = "id";
+            id.ReadOnly = true;
+            // 
+            // usedRAM
+            // 
+            usedRAM.HeaderText = "ОЗУ";
+            usedRAM.Name = "usedRAM";
+            usedRAM.ReadOnly = true;
+            // 
+            // startTime
+            // 
+            startTime.HeaderText = "Время запуска";
+            startTime.Name = "startTime";
+            startTime.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Диспетчер";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn number;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn id;
+        private DataGridViewTextBoxColumn usedRAM;
+        private DataGridViewTextBoxColumn startTime;
     }
 }
--- ProcessDispatcher/Form1.cs
+++ ProcessDispatcher/Form1.cs
@@ -1,3 +1,9 @@
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.Management;
+
+
+
 namespace ProcessDispatcher
 {
     public partial class Form1 : Form
@@ -6,5 +12,109 @@ namespace ProcessDispatcher
         {
             InitializeComponent();
         }
+        private void PrintAllProcessList()
+        {
+            int num = 1;
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Process");
+                ManagementObjectCollection processes = searcher.Get();
+
+                foreach (ManagementObject process in processes)
+                {
+                    string processName = process["Name"].ToString();
+                    int processId = int.Parse(process["ProcessId"].ToString());
+                    long pagedMemorySize = long.Parse(process["WorkingSetSize"].ToString());
+                    DateTime startTime = ManagementDateTimeConverter.ToDateTime(process["CreationDate"].ToString());
+
+                    dataGridView1.Rows.Add(num, processName, processId, pagedMemorySize, startTime);
+                    num++;
+                }
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add("Ошибка", ex.Message);
+            }
+        }
+
+        private void GetProcessButton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            PrintAllProcessList();
+        }
+
+        private void RefreshProcessListButton_Click(Object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                PrintAllProcessList();
+            }
+
+            catch (Exception ex)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void KillProcessButton_Click(object sender, EventArgs e)
+        {
+            int processId = (int)dataGridView1.SelectedCells[0].Value;
+            Process process = Process.GetProcessById(processId);
+            try
+            {
+                process.Kill();
+            }
+
+            catch ( Exception ex ) 
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add($"Ошибка: {ex.Message}");
+            }
+            dataGridView1.Rows.Clear();
+            PrintAllProcessList();
+        }
+
+        private void Load_Form1()
+        {
+            dataGridView1.Rows.Clear();
+            PrintAllProcessList();
+        }
+
+        private void SortByProcessName()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Process");
+            ManagementObjectCollection processes = searcher.Get();
+
+            var sortedProcesses = processes.Cast<ManagementObject>().OrderBy(process => process["Name"].ToString());
+
+            dataGridView1.Rows.Clear();
+
+            int num = 1;
+
+            try
+            {
+                foreach (ManagementObject process in processes)
+                {
+                    string processName = process["Name"].ToString();
+                    int processId = int.Parse(process["ProcessId"].ToString());
+                    long pagedMemorySize = long.Parse(process["WorkingSetSize"].ToString());
+                    DateTime startTime = ManagementDateTimeConverter.ToDateTime(process["CreationDate"].ToString());
+
+                    dataGridView1.Rows.Add(num, processName, processId, pagedMemorySize, startTime);
+                    num++;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.Add("Ошибка", ex.Message);
+            }
+        }
+
+
     }
 }
